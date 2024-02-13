78365bf3be5e23a1e9c683cca87e9df62bb07c25
hewensong01 <hewensong01@baidu.com>
2024-02-13 12:40:18 +0800

    update registry address

--- src/404/index.tsx
+++ src/404/index.tsx
@@ -1,8 +1,10 @@
+import './index.less'
+
 const NotFound = () => {
   return (
     <div className="not-found">
       <h1>404</h1>
-      <p>Page not found.</p>
+      <p className='title'>Page not found.</p>
     </div>
   );
 };
